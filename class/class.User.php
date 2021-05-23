418a4b0c9723ca5d26803eb785c89cd400c10ab7
AbdurrahamanFaqih01 <abdurrahamanfaqih11@gmail.com>
2021-05-23 15:00:12 +0700

    Edit User

--- class/class.User.php
+++ class/class.User.php
@@ -10,6 +10,7 @@ private $role ='';
 private $emp ;
 private $hasil = false;
 private $message ='';
+
 public function __get($atribute)
 {
     if
@@ -18,55 +19,33 @@ public function __get($atribute)
         return $this->$atribute;
     }
 }
-public function __set(
-$atribut
-
-, $value){
-
-if
-(property_exists
-($this
-,
-$atribut))
-{
-
-$this
--
->
-$atribut
-
-= $value
-;
-
+public function __set($atribut, $value){ 
+    if(property_exists($this,$atribut)){ 
+        $this->$atribut= $value; 
+    } 
 }
+
+public function AddUser(){ 
+    $sql = "INSERT INTO user(email, password, name, role) values('$this->email', '$this->password', '$this->name', '$this->role')"; 
+    $this->hasil = mysqli_query($this->connection, $sql); 
+    if($this->hasil) 
+        $this->message ='Data berhasil ditambahkan!'; 
+    else 
+        $this->message ='Data gagal ditambahkan!'; 
+} 
+    
+public function ValidateEmail($inputemail){ 
+    $sql = "SELECT * FROM user WHERE email = '$inputemail'"; 
+    $result = mysqli_query($this->connection, $sql); 
+    if (mysqli_num_rows ($result) == 1){ 
+        $this->hasil = true; 
+        $data = mysqli_fetch_assoc($result); 
+        $this->userid = $data['userid']; 
+        $this->password = $data['password']; 
+        $this->name=$data['name']; 
+        $this->email=$data['email']; 
+        $this->role=$data['role']; 
+    } 
+}        
 }
-public function AddUser(){
-    $sql = "INSERT INTO user(email, password, name, role)
-    
-    values ('$this->email', '$this->password’, '$this->name', '$this->role')";
-    
-    $this->hasil = mysqli_query($this->connection, $sql);
-    if($this->hasil)
-    $this->message ='Data berhasil ditambahkan!';
-    else
-    $this->message ='Data gagal ditambahkan!';
-    }
-    public function ValidateEmail($inputemail){
-    
-    $sql = "SELECT * FROM user
-    
-    WHERE email = '$inputemail'";
-    
-    $result = mysqli_query($this->connection, $sql);
-    if (mysqli_num_rows ($result) == 1){
-    $this->hasil = true;
-    $data = mysqli_fetch_assoc($result);
-    $this->userid = $data['userid'];
-    $this->password = $data['password'];
-    $this->name=$data[‘name'];
-    $this->email=$data['email'];
-    $this->role=$data['role'];
-    }
-    }
-    }
-    ?>
+?>
